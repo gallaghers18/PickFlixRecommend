36d08e231366fae02f8492e0deff5d89b29e1aeb
gallaghers18 <gallaghers@carleton.edu>
2018-12-08 10:58:10 -0800

    updates

--- main.py
+++ main.py
@@ -16,11 +16,31 @@ params = {
     'include_adult': 'false',
     'include_video': 'false',
     'page': page_num
-
 }
 response = requests.get(url, params = params)
-print(response)
 movie_list = json.loads(response.content)
 
-for movie in movie_list['results']:
-    print(movie['title'])
+movie_results = movie_list['results']
+
+for i in range(2, 40):
+    page_num = i
+    params = {
+        'api_key': api_key,
+        'language': 'en-US',
+        'sort_by': 'popularity.desc',
+        'include_adult': 'false',
+        'include_video': 'false',
+        'page': page_num
+    }
+    response = requests.get(url, params=params)
+    movie_list = json.loads(response.content)
+    movie_results += movie_list['results']
+
+df = pd.DataFrame(movie_results)
+df = df.drop(labels=['adult','backdrop_path','poster_path','video'], axis=1)
+liked = np.random.rand(len(df)) < 0.05
+df['liked'] = liked
+# msk = np.random.rand(len(df)) < 0.8
+# train = df[msk]
+# test = df[~msk]
+
